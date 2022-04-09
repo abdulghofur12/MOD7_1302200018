ad027fe3876deb5ecb9a6a19baac0327ce7554a2
abdulghofur12 <ghofursans12@gmail.com>
2022-04-09 10:01:24 +0700

    Add project files.

---
+++ MOD7_1302200018/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MOD7_1302200018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello word");
+        }
+    }
+
+}
