720e4e37f2fd7dae89a4beffc5d7b28e42ce6202
abdulghofur12 <ghofursans12@gmail.com>
2022-04-09 11:13:17 +0700

    mulai hello word

--- MOD7_1302200018/Program.cs
+++ MOD7_1302200018/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 
 namespace MOD7_1302200018
 {
@@ -10,4 +12,63 @@ namespace MOD7_1302200018
         }
     }
 
+    class Config
+    {
+        public BankTransferConfig conf;
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string fileconfigName = "bank_transfer_config.json";
+       
+
+        public Config()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+
+        private Config ReadConfigFile() 
+        {
+            string jsonstringFromFile=File.ReadAllText(path+ "/" + fileconfigName);
+            conf = JsonSerializer.Deserialize<BankTransferConfig>(jsonstringFromFile);
+            
+        }
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(conf, options);
+            string fullfilepath = path + "/" + fileconfigName;
+            File.WriteAllText(fullfilepath, jsonString);
+
+        }
+
+
+        private void setDefault()
+        {
+            BankTransferConfig obj textawal = new BankTransferConfig(
+                "“Please insert the amount of money to transfe:", 
+                "“Masukkan jumlah uang yang akan di-transfe");
+            conf = new BankTransferConfig(obj textawal);
+    }
+
+    class BankTransferConfig
+    {
+        public string en { get; set;}
+        public string id { get; set;}
+
+        public BankTransferConfig(string en,string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
 }
